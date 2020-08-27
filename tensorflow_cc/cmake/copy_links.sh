b3214fe0b25051d1c567e60a106c72312b25063b
Solo Super <11663739+solosuper@users.noreply.github.com>
2020-08-27 19:03:58 +0200

    Update copy_links.sh

--- tensorflow_cc/cmake/copy_links.sh
+++ tensorflow_cc/cmake/copy_links.sh
@@ -5,18 +5,5 @@ set -e
 
 echo "Replacing links with the copies of their targets."
 echo "This may take a while..."
-# To properly handle whitespace characters in filenames, we need to use
-# an ugly `find` and `read` trick.
-find -L "$1" -depth -print0 |
-    while IFS= read -r -d $'\0' f; do
-        # We need to check whether the file is still a link.
-        # It may have happened that we have already replaced it by
-        # the original when some of its parent directories were copied.
-        # Also the first check is to detect whether the file (after
-        # symlink dereference) exists so that `realpath` does not fail.
-        if [[ -w "$f" ]] && [[ -L "$f" ]]; then
-            realf="$(realpath "$f")"
-            rm "$f"
-            cp -r --link "$realf" "$f" 2>/dev/null || cp -r "$realf" "$f"
-        fi
-    done
+rsync -amLq "${1}/" "__tmp__/"
+mv "__tmp__" "${1}"
