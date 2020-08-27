36ec41d8ec9c0ab4dada6195ce4c52357d4eb6f4
Solo Super <11663739+solosuper@users.noreply.github.com>
2020-08-27 21:27:37 +0200

    Update copy_links.sh

--- tensorflow_cc/cmake/copy_links.sh
+++ tensorflow_cc/cmake/copy_links.sh
@@ -6,5 +6,9 @@ set -euxo pipefail
 echo "Replacing links with the copies of their targets."
 echo "This may take a while..."
 rsync -amL ${1}/ __tmp__
+
+# But keep symlinks for the so-files
+rsync -al ${1}/tensorflow/libtensorflow_cc.so* __tmp__/tensorflow/
+
 rm -rf "${1}"
 mv __tmp__ bazel-bin
